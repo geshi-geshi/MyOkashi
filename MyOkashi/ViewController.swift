7f5534e5fa7839575b26ebcfeb17f3f4e7e86b8d
geshi-geshi <shige22000200021@gmail.com>
2020-04-21 18:04:46 +0900

    取得データで一覧表示した

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -1,19 +1,21 @@
 import UIKit
 
-class ViewController: UIViewController,UISearchBarDelegate {
+class ViewController: UIViewController,UISearchBarDelegate,UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         //SearchBarのdelegate通知先を設定
         searchText.delegate = self
-        
         searchText.placeholder = "お菓子の名前を入力して下さい"
+        // Table ViewのdataSourceを設定
+        tableView.dataSource = self
     }
 
     @IBOutlet weak var searchText: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    // お菓子のリスト(タプル配列)
+    var okashiList : [(maker:String, name:String, price:String, comment:String, link:URL, image:URL)] = []
     // 検索ボタンをクリックした時
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
@@ -79,7 +81,30 @@ class ViewController: UIViewController,UISearchBarDelegate {
                 // 受け取ったJSONデータをパース（解析）して格納
                 let json = try decoder.decode(ResultJson.self, from: data!)
                 
-                print(json)
+//                print(json)
+                
+                // お菓子の取得ができているかの確認
+                if let items = json.item {
+                    // お菓子のリストを初期化
+                    self.okashiList.removeAll()
+                    // 取得しているお菓子の数だけ処理
+                    for item in items {
+                        // それぞれをアンラップ
+                        if let maker = item.maker, let name = item.name, let price = item.price, let comment = item.comment, let link = item.url, let image = item.image {
+                            // 一つのお菓子をタプルでまとめて管理
+                            let okashi = (maker, name, price, comment, link, image)
+                            // お菓子の配列へ追加
+                            self.okashiList.append(okashi)
+                        }
+                    }
+                    // Table Viewを更新する
+                    self.tableView.reloadData()
+                    
+                    if let okashidbg = self.okashiList.first {
+                        print("-----------------------")
+                        print("okashiList[0] = \(okashidbg)")
+                    }
+                }
             } catch {
                 print("エラーです!!!!")
             }
@@ -87,5 +112,28 @@ class ViewController: UIViewController,UISearchBarDelegate {
         
         // ダウンロード開始(dataTaskで登録されたるリクエストのタスクが実行される)
         task.resume()
+    }
+    
+    // Cellの総数を返すdatasourceメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // お菓子リストの総数
+        return okashiList.count
+    }
+    
+    // Cellに値を設定するdatasourceメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 表示を行うCellオブジェクトを取得
+        let cell = tableView.dequeueReusableCell(withIdentifier: "okashiCell", for: indexPath)
+        // お菓子のタイトル設定
+        cell.textLabel?.text = okashiList[indexPath.row].name
+        // お菓子画像を取得
+        if let imageData = try? Data(contentsOf: okashiList[indexPath.row].image) {
+            // 正常に取得できた場合はUIImageで画像オブジェクトを生成して、Cellにお菓子画像を設定
+            cell.imageView?.image = UIImage(data: imageData)
+        }
+        
+        // 設定済みのCellオブジェクトを画面に反映
+        return cell
+        
     }
 }
